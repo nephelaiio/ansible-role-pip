5bc53bfb415c7eea0df86f7a0d1c9a0cb03b7dd7
Ted Cook <teodoro.cook@gmail.com>
2019-05-02 23:00:43 -0600

    Revert test fixture changes

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_command(Host):
-    assert Host.command('pip --version').rc == 0
+def test_command(Command):
+    assert Command('pip --version').rc == 0
